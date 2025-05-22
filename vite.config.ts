d859f1ce8df132543d42cb432195ea6e90f9b7f1
Rasmus Tanggaard <rasmus@tthost.dk>
2025-05-22 13:12:07 +0200

    Small startup changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+
+  return (
+  {
   server: {
     host: "::",
-    port: 8080,
+    port: parseInt(env.VITE_PORT),
   },
   plugins: [
     react(),
@@ -19,4 +24,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+})});
